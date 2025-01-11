67e309f379ae9c25d7ccc488156feaab16722bfc
Raushan Kumar <raushankumar2909@gmail.com>
2025-01-12 00:03:01 +0530

    feat: add authentication middleware to verify JWT tokens

---
+++ server/middleware/auth.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const isAuth = (req:Request, res:Response, next:NextFunction)=>{
+    const token = req.headers["authorization"];
+    if(!token){
+        res.status(401).json({message:"No token, authorization denied"});
+        return;
+    }
+    try {
+        if (!process.env.JWT_SECRET) {
+            return res.status(500).json({ message: "JWT_SECRET is not defined" });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({message:"Token is not valid"});
+    }
+};
